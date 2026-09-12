2c3bc8949e27957e68f03756b2a1b948759489d7
Sagar Niroula <sagarniroula112@gmail.com>
2026-09-12 20:10:03 +0545

    Add GetAccountDetails API to AccountService

--- account-service/src/main/java/com/learning/banking/accountservice/AccountServiceImpl.java
+++ account-service/src/main/java/com/learning/banking/accountservice/AccountServiceImpl.java
@@ -1,8 +1,6 @@
 package com.learning.banking.accountservice;
 
-import com.learning.banking.account.v1.AccountServiceGrpc;
-import com.learning.banking.account.v1.GetAccountBalanceRequest;
-import com.learning.banking.account.v1.GetAccountBalanceResponse;
+import com.learning.banking.account.v1.*;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
@@ -11,12 +9,31 @@ public class AccountServiceImpl
         extends AccountServiceGrpc.AccountServiceImplBase {
 
     @Override
+    public void getAccountDetails(
+            GetAccountDetailsRequest request,
+            StreamObserver<GetAccountDetailsResponse> responseObserver) {
+
+        String accountId = request.getAccountId();
+        System.out.println("Received request for account: " + accountId);
+
+        GetAccountDetailsResponse response =
+                GetAccountDetailsResponse.newBuilder()
+                        .setAccountId(accountId)
+                        .setSchemeCode("SVZA")
+                        .setCustomerAccountType("Savings")
+                        .setBranchCode(41)
+                        .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
     public void getAccountBalance(
             GetAccountBalanceRequest request,
             StreamObserver<GetAccountBalanceResponse> responseObserver) {
 
         String accountId = request.getAccountId();
-
         System.out.println("Received request for account: " + accountId);
 
         GetAccountBalanceResponse response =
